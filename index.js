998c619ef788eebc7e1876d47404cd771098da8a
Sonja <sonjaljatkovic@gmail.com>
2023-10-09 18:23:03 -0400

    added option to choose license

--- index.js
+++ index.js
@@ -9,31 +9,26 @@ const questions = [
         type: "checkbox",
         name: "license",
         message: "Please choose a license for the project:",
-        choices: ["MIT", "Apache", "Boost", "BSD 3-Clause License", "Eclipse",
-        "GNU GPL v3", "IBM", "ISC", "Mozilla"],
-        validate: your_licence => {
-            if (your_licence) {
-                return true;
-            } else {
-                console.log("Select the license:");
-                return false;
-            }
-        }
+        choices: ["MIT", "Apache", "Boost", "BSD 3-Clause License", "Eclipse", "GNU GPL v3", "IBM", "ISC", "Mozilla"],
     },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+    fs.writeFile(fileName, data, (error) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Your README file has been created.");
+    });
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
     .then(function (userInput) {
-        console.log(userInput)
         writeToFile("README.md", generateMarkdown(userInput));
-    })
+    });
 }
 
 // Function call to initialize app
